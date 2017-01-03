0f214f7b0352420d39d9c07f6cb383f1c18f9dc1
Fabian Schaffert <fabian@schaffert.cc>
2017-01-03 11:41:18 +0100

    Add test for Hero binding

---
+++ hero_service/tests/hero_binding_tests.py
@@ -0,0 +1,48 @@
+import json
+
+from channels import Group
+from channels.binding.base import UPDATE
+from channels.tests import ChannelTestCase, HttpClient
+
+from ..consumers import Demultiplexer
+from .. import models
+
+class HeroBindingTests(ChannelTestCase):
+
+    def test_create_hero(self):
+        Group("hero-updates").add("hero-reply")
+        hero = models.Hero.objects.create(name='My hero')
+
+        message = self.get_next_message('hero-reply', require=True)
+
+        content = json.loads(message['text'])
+        self.assertEqual(content['stream'], 'hero')
+
+        payload = content['payload']
+        self.assertEqual(payload['model'], 'hero_service.hero')
+        self.assertEqual(payload['action'], 'create')
+        self.assertEqual(payload['pk'], hero.pk)
+
+        data = payload['data']
+        self.assertEqual(data['name'], hero.name)
+
+    def test_update_hero(self):
+        hero = models.Hero.objects.create(name='My hero')
+
+        payload = {
+            'action': UPDATE,
+            'pk': hero.pk,
+            'data': {
+                'name': 'Still my hero',
+            },
+        }
+
+        client = HttpClient()
+        client.send_and_consume('websocket.receive', path='/api/ws', text={
+            'stream': 'hero',
+            'payload': payload,
+        })
+        client.consume('binding.hero')
+
+        hero.refresh_from_db()
+        self.assertEqual(hero.name, 'Still my hero')
