65fd4bf8bdd10b314d894453f2c3f9298d22bd81
Fabian Schaffert <fabian@schaffert.cc>
2017-01-03 10:02:02 +0100

    Add Hero model

--- hero_service/models.py
+++ hero_service/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Hero(models.Model):
+    """
+    Represents a Hero.
+    """
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
